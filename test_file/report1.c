0590841c6676d693ed61acae1924fbf4c85e2e09
yinsong <yinsong@geely.com>
2019-02-27 15:07:43 +0800

    first commit

---
+++ test_file/report1.c
@@ -0,0 +1,9 @@
+
+
+=================================Documents/Cpplint/code/1/report1.txt===================================
+
+python: can't open file '/Documents/Cpplint/code/cpplint.py': [Errno 2] No such file or directory
+
+=================================Documents/Cpplint/code/1/report.txt===================================
+
+python: can't open file '/Documents/Cpplint/code/cpplint.py': [Errno 2] No such file or directory
